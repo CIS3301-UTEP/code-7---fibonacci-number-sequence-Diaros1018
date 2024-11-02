051ec4d1f5369c2f913d884fe4d2e2b5e6ffeae4
Michigan <dlyanez@miners.utep.edu>
2024-11-01 18:22:27 -0600

    test 2

--- code_12.py
+++ code_12.py
@@ -1,17 +1,18 @@
-import random
+
 def get_fibonacci_number(position):
-   if position <= 1:
+   if position == 1:
        return 1
-   return get_fibonacci_number(position - 1) + get_fibonacci_number(position - 2)
+   elif position == 2:
+       return 1
+   else:
+       return get_fibonacci_number(position - 1) + get_fibonacci_number(position - 2)
    
 def get_fibonacci_number_sequence(number):
-    return [get_fibonacci_number(i) for i in range (number)]
-def check_list():
-    return True
-    
+    return [get_fibonacci_number(i) for i in range (1, number + 1)]
+   
 
 if __name__ == "__main__":
-    check_list ()
-    # n = 7 
-    # print(f"The fibonacci for {n} number is {get_fibonacci_number_sequence(n)} ")
+    
+    n = 7 
+    print(f"The fibonacci for {n} number is {get_fibonacci_number_sequence(n)} ")
     
